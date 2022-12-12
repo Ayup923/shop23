e5d23f4b886352f0c79a718040817efb4e415d3b
Ayup923 <aupmaan@gmail.com>
2022-12-12 12:09:18 +0600

    permissions

--- review/views.py
+++ review/views.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CommentSerializer, RatingSerializer
 from .models import Comment, Rating
+from .permissions import IsAuthOrReadOnly
+
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthOrReadOnly]
 
 
 class RatingViewSet(ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    permission_classes = [IsAuthOrReadOnly]    
